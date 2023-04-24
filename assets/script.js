26a0ed469abe3321531d896d6894c5493ca9b845
Potatonaut <stam296@gmail.com>
2023-04-23 21:23:54 -0500

    created five multiple choice questions with answer choices in array in javascript

---
+++ assets/script.js
@@ -0,0 +1,51 @@
+// Array of questions and answer choices
+let questions = [
+    {
+        question: "What CSS pseudo-class is needed to declare variables in CSS?",
+        choiceA: "*",
+        choiceB: "var",
+        choiceC: ":root",
+        choiceD: ":var",
+        correctChoice: "C"
+    },
+    {
+        question: "What does 'this' in Javascript refer to?",
+        choiceA: "The global object",
+        choiceB: "The current element",
+        choiceC: "The parent element",
+        choiceD: "None of the above",
+        correctChoice: "A"
+    },
+    {
+        question: "What is 'function' and 'var known as?",
+        choiceA: "Data types",
+        choiceB: "Logical comparators",
+        choiceC: "Objects",
+        choiceD: "Declaration statements",
+        correctChoice: "D"
+    },
+    {
+        question: "If a local variable and global variable have the same name, which one takes precedence?",
+        choiceA: "Global variable",
+        choiceB: "Local variable",
+        choiceC: "Both global and local variable",
+        choiceD: "None of the above",
+        correctChoice: "B"
+    },
+    {
+        question: "What is the correct order of HTML tags for a webpage?",
+        choiceA: "HTML, Head, Title, Body",
+        choiceB: "HTML, Title, Head, Body",
+        choiceC: "Head, Title, Body, HTML",
+        choiceD: "Title, Head, Body, HTML",
+        correctChoice: "A" 
+    }
+]
+
+// make navigate function that goes to next question once current one is answered
+
+// need  to localstorage the score and name of user
+
+//make the multiple choice answers buttons
+
+//need timer
